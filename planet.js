1c387eadca00582b79a704c8d9d87f333eab4778
vocispopulus <vocispopulus@users.noreply.github.com>
2016-06-01 23:02:36 +0100

    fiddle with plugin setup

--- planet.js
+++ planet.js
@@ -1,10 +1,12 @@
-    var planet = planetaryjs.planet();
-    // You can remove this statement if `world-110m.json`
-    // is in the same path as the HTML page:
-    //planet.loadPlugin(planetaryjs.plugins.earth({
-    //  topojson: { file: 'world-110m.json' }
-    //}));
-    // Make the planet fit well in its canvas
-    planet.projection.scale(250).translate([250, 250]);
-    var canvas = document.getElementById('globe');
-    planet.draw(canvas);
+// Setup stuff
+planetaryjs.plugins.earth({
+  topojson: { file:   'world-110m.json' },
+  oceans:   { fill:   '#000080' },
+  land:     { fill:   '#339966' },
+  borders:  { stroke: '#008000' }
+});
+var planet = planetaryjs.planet();
+// Make the planet fit well in its canvas
+planet.projection.scale(250).translate([250, 250]);
+var canvas = document.getElementById('globe');
+planet.draw(canvas);
